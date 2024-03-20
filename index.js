045657c3a06aa44c3cbbfc6b36e2b234b9364ed6
prachigoel112 <144596623+prachigoel112@users.noreply.github.com>
2024-03-20 21:04:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+
+}
+
+
+
+function cleardisplay(input) {
+    display.value = "";
+}
+
+
+
+function calculate(input) {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+    }
+
+}
